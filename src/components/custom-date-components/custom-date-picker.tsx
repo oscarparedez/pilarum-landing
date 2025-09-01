4a32e59e15cb4d4af1e1b5c022ab801f60dd490c
oscarparedez <oaparedez17@gmail.com>
2025-09-01 12:10:55 -0600

    update date picker styles

--- src/components/custom-date-components/custom-date-picker.tsx
+++ src/components/custom-date-components/custom-date-picker.tsx
@@ -3,16 +3,37 @@ import { DatePicker, DatePickerProps } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { es } from 'date-fns/locale';
+import { SxProps, Theme } from '@mui/material/styles';
+import { GlobalStyles } from '@mui/material';
 
-interface CustomDatePickerProps extends DatePickerProps<Date> {}
+interface CustomDatePickerProps extends DatePickerProps<Date> {
+  sx?: SxProps<Theme>;
+}
 
-export const CustomDatePicker: FC<CustomDatePickerProps> = (props) => {
+export const CustomDatePicker: FC<CustomDatePickerProps> = ({ sx, ...props }) => {
   return (
-    <LocalizationProvider
-      dateAdapter={AdapterDateFns}
-      adapterLocale={es}
-    >
-      <DatePicker {...props} />
-    </LocalizationProvider>
+    <>
+      <GlobalStyles
+        styles={{
+          '.MuiPickersPopper-root': {
+            '& .MuiPickersCalendarHeader-label': {
+              textTransform: 'capitalize !important',
+            },
+            '& .MuiDayCalendar-weekDayLabel': {
+              textTransform: 'capitalize !important',
+            },
+          },
+        }}
+      />
+      <LocalizationProvider
+        dateAdapter={AdapterDateFns}
+        adapterLocale={es}
+      >
+        <DatePicker
+          {...props}
+          sx={sx}
+        />
+      </LocalizationProvider>
+    </>
   );
 };
